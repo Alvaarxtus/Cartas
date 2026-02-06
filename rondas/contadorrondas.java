77cecd785929a57724ceae045eca5c19aca2ffdf
Alvaarxtus <alvaro.carretero@gracia.lasalle.cat>
2026-02-06 12:37:13 +0100

    rondas

--- rondas/contadorrondas.java
+++ rondas/contadorrondas.java
@@ -1,5 +1,12 @@
 package rondas;
 
+import jugadores.jugadores;
+import cartas.cartas;
+
 public class contadorrondas {
-    
+
+    public static void pasarRonda(jugadores j) {
+        j.getCartasEnMesa().removeIf(cartas::estaMuerta);
+    }
 }
+
